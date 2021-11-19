387da437102165a0768a1037b4df11bbe570bd1f
Daniel Bark <danba340@gmail.com>
2021-11-19 17:47:36 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useGamepads } from 'react-gamepads';
 import './App.css';
 
+const buttonLabels = [
+  "A",
+  "B",
+  "X",
+  "Y",
+  "L1",
+  "R1",
+  "L2",
+  "R2",
+  "Back",
+  "Start",
+  "L3",
+  "R3",
+  "UP",
+  "DOWN",
+  "LEFT",
+  "RIGHT",
+  "XBOX",
+]
+
+const axesLabels = [
+  "LX",
+  "LY",
+  "RX",
+  "RY",
+]
+
 function App() {
+  const [gamepads, setGamepads] = useState([]);
+  useGamepads(_gamepads => {
+    setGamepads(Object.values(_gamepads))
+  })
+  if (!gamepads) return '';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{ background: `rgb(${128 + (gamepads[0].axes[0] * 128)},128,128)` }}
+    >
+      {gamepads.length && gamepads.map(gp => {
+        return (
+          <div>
+            <div><span>ID:</span>{gp.id}</div>
+            {gp.buttons.map((button, index) => {
+              return (
+                <div><span>{buttonLabels[index]}:</span><span>{button.value}</span></div>
+              )
+            })}
+            {gp.axes.map((stick, index) => {
+              return (
+                <div><span>{axesLabels[index]}:</span><span>{stick}</span></div>
+              )
+            })}
+          </div>
+        )
+      })}
     </div>
   );
 }
